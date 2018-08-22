480034ddbc2298e4da3be5ec252a5e319aa3c6c7
david <davhans@microsoft.com>
2018-08-22 14:00:23 -0500

    initialupload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function startLogic() {
+}
+startLogic();
+    function clickHandler() {
+    alert('A Fine Choice');
+}
+startLogic();
+    function clickHandler1() {
+    alert('Not A Bad Choice But Are You Sure You Don\'t Want BBQ?');
+}
+startLogic();
+    function clickHandler2() {
+    alert('You\'re Dead To Me');
+}
